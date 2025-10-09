c69f7d9264faf5bde0eb790c6a1b3540a6b6f26f
1107 <1107@siwg.top>
2025-10-09 22:42:14 +0100

    Updated Quat Code

---
+++ Drone/code.c
@@ -0,0 +1,42 @@
+#include <math.h>
+
+typedef struct {
+    double w;
+    double x;
+    double y;
+    double z;
+} Quaternion;
+
+Quaternion quatMultiply(Quaternion q1, Quaternion q2) {
+    Quaternion res;
+    res.w = q1.w*q2.w - q1.x*q2.x - q1.y*q2.y - q1.z*q2.z;
+    res.x = q1.w*q2.x + q1.x*q2.w + q1.y*q2.z - q1.z*q2.y;
+    res.y = q1.w*q2.y - q1.x*q2.z + q1.y*q2.w + q1.z*q2.x;
+    res.z = q1.w*q2.z + q1.x*q2.y - q1.y*q2.x + q1.z*q2.w;
+    return res;
+}
+
+void quatNormalize(Quaternion *q) {
+    double norm = sqrt(q->w*q->w + q->x*q->x + q->y*q->y + q->z*q->z);
+    q->w /= norm;
+    q->x /= norm;
+    q->y /= norm;
+    q->z /= norm;
+}
+
+void quadGyroUpdate(Quaternion *q, double gx, double gy, double gz, double dt) {
+    Quaternion q_gyro = {0, gx, gy, gz};
+    //运算
+    Quaternion q_tnext = quatMultiply(*q, q_gyro);
+    q_tnext.w *= 0.5;
+    q_tnext.x *= 0.5;
+    q_tnext.y *= 0.5;
+    q_tnext.z *= 0.5;
+
+    q_gyro.w += q_tnext.w * dt;
+    q_gyro.x += q_tnext.x * dt;
+    q_gyro.y += q_tnext.y * dt;
+    q_gyro.z += q_tnext.z * dt;
+    //归一
+    quatNormalize(q);
+}
