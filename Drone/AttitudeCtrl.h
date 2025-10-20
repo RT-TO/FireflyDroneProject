091b6ebe96795a0586101fe3ca918bc40788c4e7
1107 <1107@siwg.top>
2025-10-20 22:13:36 +0100

    qwq

--- Drone/AttitudeCtrl.h
+++ Drone/AttitudeCtrl.h
@@ -4,8 +4,8 @@
 #include "quaternion.h"
 
 void quatGyroUpdate(Quaternion *q, double gx, double gy, double gz, double dt);
-void acceAngleComp(Quaternion *q, double ax, double ay, double az, double dt);
 Quaternion bodyToEarth(Quaternion q, Quaternion v_b);
 double getAltitude(double pressure, double temperature);
+void MEKF_filter(double dt, const double Q[3][3], const double R[3][3]);
 
 #endif
